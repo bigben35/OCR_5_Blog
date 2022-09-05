1adef651d348f8b44006c43d0817e7fd387233d0
Josselin Crenn <josselincrenn@gmail.com>
2022-09-05 13:34:16 +0200

    improve MVC (creating layouts)

---
+++ src/Views/Front/layouts/footer.php
@@ -0,0 +1,58 @@
+ <!-- Footer -->
+ <footer class="text-center">
+     <div class="footer-above">
+         <div class="container">
+             <div class="row">
+                 <div class="footer-col col-md-12">
+                     <h3>Me Trouver</h3>
+                     <ul class="list-inline">
+                         <li>
+                             <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                         </li>
+                         <li>
+                             <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-github"></i></a>
+                         </li>
+                         <li>
+                             <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                         </li>
+                        
+                     </ul>
+                 </div>
+                 <!-- <div class="footer-col col-md-6">
+                     <h3>About Freelancer</h3>
+                     <p>Freelance is a free to use, open source Bootstrap theme created by <a
+                             href="http://startbootstrap.com">Start Bootstrap</a>.</p>
+                 </div> -->
+             </div>
+         </div>
+     </div>
+     <div class="footer-below">
+         <div class="container">
+             <div class="row">
+                 <div class="col-lg-12">
+                     Copyright &copy; - Web Code - <?= date("Y")?>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </footer>
+
+ <!-- jQuery -->
+ <script src="Public/vendor/jquery/jquery.min.js"></script>
+
+ <!-- Bootstrap Core JavaScript -->
+ <script src="Public/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+ <!-- Plugin JavaScript -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+ <!-- Contact Form JavaScript -->
+ <script src="Public/js/jqBootstrapValidation.js"></script>
+ <script src="Public/js/contact_me.js"></script>
+
+ <!-- Theme JavaScript -->
+ <script src="Public/js/freelancer.min.js"></script>
+
+ </body>
+
+ </html>
